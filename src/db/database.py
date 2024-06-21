bc8709a0329ed3a402ec9b8335a16c6389febf7c
Neha-Gi <neha.girme@dci-student.org>
2024-06-21 11:32:25 +0200

    added requirements

--- src/db/database.py
+++ src/db/database.py
@@ -1,7 +1,15 @@
 import psycopg as pg
 
 #Singleton 
-class Database:
+class Database(object):
+    __instance = None
+
+    def __new__(cls):
+        if Database.__instance is None:
+            Database.__instance = super().__new__(cls)
+            Database.__instance.__init__()
+
+        return Database.__instance.__conn    
     def __init__(self) -> None:
         self.__conn = pg.connect(
             #host = 'localhost'
